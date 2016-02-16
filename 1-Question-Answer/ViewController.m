4741f74449888caa6d4bdde61b9ada7869702164
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-02-16 22:20:48 +1100

    Added files

--- 1-Question-Answer/ViewController.m
+++ 1-Question-Answer/ViewController.m
@@ -24,4 +24,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) presentAnswer: (NSString*) title boolCorrect:(NSString*) boolCorrect {
+    UIAlertController *incorrect = [UIAlertController alertControllerWithTitle:title message:boolCorrect preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *close = [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleDefault handler:nil];
+    [incorrect addAction:close];
+    [self presentViewController:incorrect animated:YES completion:nil];
+}
+
+- (IBAction)onQuestionPressed:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Question" message:@"Is Melbourne a city in Australia?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *yes = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self presentAnswer:@"Correct" boolCorrect:@"This is correct."];
+    }];
+    UIAlertAction *no = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self presentAnswer:@"Incorrect" boolCorrect:@"That is incorrect."];
+    }];
+    [alert addAction:yes];
+    [alert addAction:no];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
 @end
